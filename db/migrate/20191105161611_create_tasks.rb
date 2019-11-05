aaa215a34b491522267eb1b51ac486530f7b526b
Joe Slack <jslack2537@gmail.com>
2019-11-05 16:24:57 +0000

    added tests gems and tasks controller

--- db/migrate/20191105161611_create_tasks.rb
+++ db/migrate/20191105161611_create_tasks.rb
@@ -1,6 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
+      t.string :title
+      t.boolean :done, default: false
 
       t.timestamps
     end
